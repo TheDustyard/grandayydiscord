de9e03b159cbfc68691c18f9446512ee201a0e16
Zachary Kohnen <DusterTheFirst@users.noreply.github.com>
2018-01-09 23:03:07 -0500

    no

--- pages/profile/profilescroller.js
+++ pages/profile/profilescroller.js
@@ -1,25 +1,39 @@
 window.onload = () => {
     if (document.documentElement.clientWidth < 1040) {
-        return
-    }
+        // Mobile
 
-    let rolemenu = document.querySelector('div.pfp');
-    let profile = document.querySelector('div.profile');
-    let rolemenutop = rolemenu.getBoundingClientRect().top;
+        let backphoto = document.querySelector('div.backphoto');
+        let picture = document.querySelector('img.picture');
+        let circle = document.querySelector('div.circle');
 
-    window.onscroll = (e) => {
-        let offset = Math.abs(document.body.getBoundingClientRect().top - 100);
-        
-        if ((offset) >= rolemenutop) {
-            if ((offset - 50) + rolemenu.getBoundingClientRect().height < profile.getBoundingClientRect().height)
-                rolemenu.style.top = `${offset - 100}px`;
-            else {
-                rolemenu.style.top = `${profile.getBoundingClientRect().height - rolemenu.getBoundingClientRect().height - 50}px`;
+        window.onscroll = (e) => {
+            let offset = Math.abs(document.body.getBoundingClientRect().top);
+            
+            console.log(offset);
+        }
+
+    } else {
+        // Desktop
+
+        let rolemenu = document.querySelector('div.pfp');
+        let profile = document.querySelector('div.profile');
+        let rolemenutop = rolemenu.getBoundingClientRect().top;
+
+        window.onscroll = (e) => {
+            let offset = Math.abs(document.body.getBoundingClientRect().top - 100);
+            
+            if ((offset) >= rolemenutop) {
+                if ((offset - 50) + rolemenu.getBoundingClientRect().height < profile.getBoundingClientRect().height)
+                    rolemenu.style.top = `${offset - 100}px`;
+                else {
+                    rolemenu.style.top = `${profile.getBoundingClientRect().height - rolemenu.getBoundingClientRect().height - 50}px`;
+                }
+            } else {
+                rolemenu.removeAttribute('style');
             }
-        } else {
-            rolemenu.removeAttribute('style');
         }
     }
-
-    window.onscroll();
+    
+    if (window.onscroll)
+        window.onscroll();
 }
